218cd2eb6024fd8a3cfdab8832882cedff14ef6d
Jose <jhcferreira444@gmail.com>
2024-10-26 01:03:01 -0300

    primeiro commit

---
+++ front/next.config.mjs
@@ -0,0 +1,17 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "img.freepik.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com", // Adicione o Unsplash aqui também
+      },
+    ],
+  },
+};
+
+export default nextConfig;
